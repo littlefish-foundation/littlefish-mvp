8c92e7fab7ddda43ec74107e354c8fbafc71ca58
Donald Isufi <70485619+Nald97@users.noreply.github.com>
2022-08-07 22:07:14 +0300

    debugging

--- frontend/src/components/CollectionCard.js
+++ frontend/src/components/CollectionCard.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./CollectionCard.css";
 
-const CollectionCard = ({ id, name, description, image }) => {
+const CollectionCard = ({ id, asset_name, description, image }) => {
   return (
     <div className="collectionCard">
       <img src={image} alt="" />
       <div className="details">
-      
         <div className="name">
-          {name} <div className="id">.#{id}</div>
+          {asset_name} <div className="id">.#{id}</div>
         </div>
       </div>
     </div>

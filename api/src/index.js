e3072b954a37e9387a6f3490fa8e16e9bb42f778
buraksenn <buraksenb@gmail.com>
2022-08-19 18:02:35 +0300

    increased payload size limit

--- api/src/index.js
+++ api/src/index.js
@@ -26,8 +26,8 @@ app.use(cors(options));
 
 app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 app.use('/', routes);
 app.use((req, res, next) => {

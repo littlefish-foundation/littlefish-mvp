c311d3be5f3c73dc81f6c638c83723d30992257f
Donald Isufi <70485619+Nald97@users.noreply.github.com>
2022-08-29 16:08:02 +0300

    Made the whole Action_Card clickable

--- frontend/src/components/ui/Nft-card/NftCard.jsx
+++ frontend/src/components/ui/Nft-card/NftCard.jsx
@@ -1,4 +1,4 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./nft-card.css";
@@ -10,50 +10,43 @@ const NftCard = (props) => {
   const [showModal, setShowModal] = useState(false);
   //const { NFT__DATA } = useFetch("https://api.littlefish.foundation/action");
 
-
   return (
-    <div className="single__nft__card">
-      <div className="nft__img">
-        <img src={image} alt="" className="w-100" />
-      </div>
+    <Link to={`/action/${assetName}`} style={{ textDecoration: "none" }}>
+      <div className="single__nft__card">
+        <div className="nft__img">
+          <img src={image} alt="" className="w-100" />
+        </div>
 
-      <div className="nft__content">
-        <h5 className="nft__title">
-          <Link to={`/action/${assetName}`}>{assetName}</Link>
-        </h5>
+        <div className="nft__content">
+          <h5 className="nft__title">{assetName}</h5>
 
-        <div className="creator__info-wrapper d-flex gap-3">
+          <div className="creator__info-wrapper d-flex gap-3">
+            <div className="creator__info w-100 d-flex align-items-center justify-content-between">
+              <div>
+                <h6>Created By</h6>
+                <p>{ownerName}</p>
+              </div>
 
-          <div className="creator__info w-100 d-flex align-items-center justify-content-between">
-            <div>
-              <h6>Created By</h6>
-              <p>{ownerName}</p>
-            </div>
-
-            <div>
-              <h6>Action Price</h6>
-              <h6>{`${price} ADA`}</h6>
-              
+              <div>
+                <h6>Action Price</h6>
+                <h6>{`${price} ADA`}</h6>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className=" mt-3 d-flex align-items-center justify-content-between">
-          <button
-            className="bid__btn d-flex align-items-center gap-1"
-            onClick={() => {
-            
-            } }
-          >
-            <i className="ri-shopping-bag-line"></i> Buy Action
-          </button>
+          <div className=" mt-3 d-flex align-items-center justify-content-between">
+            <button
+              className="bid__btn d-flex align-items-center gap-1"
+              onClick={() => {}}
+            >
+              <i className="ri-shopping-bag-line"></i> Buy Action
+            </button>
 
-          {showModal && <Modal setShowModal={setShowModal} />}
-
-         
+            {showModal && <Modal setShowModal={setShowModal} />}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
